a20ad9ca2f42a24b56d0519acc3fdaa446c9489c
Kirill <izranovks@gmail.com>
2026-09-02 12:54:02 +0800

    fix: application class moved

--- src/main/java/com/internetcafe/InternetCafeNetworkBackendApplication.java
+++ src/main/java/com/internetcafe/InternetCafeNetworkBackendApplication.java
@@ -1,4 +1,4 @@
-package com.internetcafe.repository;
+package com.internetcafe;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
