693273542ca6c8ecfee0f0d6360f590f647f54fd
Kirill <izranovks@gmail.com>
2026-09-06 00:44:11 +0800

    fix/office repository reset

--- src/main/java/com/internetcafe/repository/OfficeRepository.java
+++ src/main/java/com/internetcafe/repository/OfficeRepository.java
@@ -1,16 +1,9 @@
 package com.internetcafe.repository;
 
-import com.internetcafe.entity.Device;
 import com.internetcafe.entity.Office;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface OfficeRepository extends JpaRepository<Office, String> {
-    List<Office> getId(String officeId);
-
 }
